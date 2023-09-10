e05255d85ab50c44b41f947a5773b7d6478226ff
Tanjina sultana <tanjinasultana388@gmail.com>
2023-09-10 22:17:12 +0600

    first commit

---
+++ react-world/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Country from './components/country/Country'
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <h1>React World Tour</h1>
+        <Country></Country>
+        
+    </>
+  )
+}
+
+export default App
---
+++ react-world/src/components/country/Country.jsx
@@ -0,0 +1,10 @@
+
+const Country = () => {
+    return (
+        <div>
+            <h1>country</h1>
+        </div>
+    );
+};
+
+export default Country;
